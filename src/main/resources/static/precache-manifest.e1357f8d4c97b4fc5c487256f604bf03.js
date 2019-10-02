7e4d6d7aaff1cd66fbe9635f83f910252ceafba2
Alexander <savaleks@gmail.com>
2019-10-02 16:43:30 +0300

    added react

---
+++ src/main/resources/static/precache-manifest.e1357f8d4c97b4fc5c487256f604bf03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3265db8f887bd9ece4271a0977fb40ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48eb32296874b1adc2ee",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "786d43b5d6f21179c7f8",
+    "url": "/static/css/main.71a63cf9.chunk.css"
+  },
+  {
+    "revision": "48eb32296874b1adc2ee",
+    "url": "/static/js/2.df0fd9c4.chunk.js"
+  },
+  {
+    "revision": "786d43b5d6f21179c7f8",
+    "url": "/static/js/main.40cd6d96.chunk.js"
+  },
+  {
+    "revision": "493637d37e57c847f49b",
+    "url": "/static/js/runtime~main.c48905f3.js"
+  }
+]);
