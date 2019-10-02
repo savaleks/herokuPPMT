011931bc192ae4eacfa8c24cdce64c279924c5f5
Alexander <savaleks@gmail.com>
2019-10-02 15:18:09 +0300

    xxx

--- src/main/java/com/savaleks/ppmtool/security/SecurityConst.java
+++ src/main/java/com/savaleks/ppmtool/security/SecurityConst.java
@@ -7,5 +7,5 @@ public class SecurityConst {
     public static final String SECRET_KEY = "savaleks";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final long EXPIRATION_TIME = 300_000; // 5min
+    public static final long EXPIRATION_TIME = 3000000; // 5min
 }
